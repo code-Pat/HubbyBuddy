ae5f0e8c3ee55eaaafd16d2fb14cc91d992b1596
Donggeun Lee <patlee997@gmail.com>
2022-01-19 16:43:21 +0900

    re-organized

---
+++ HubbyBuddy/Controllers/SMSCodeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SMSCodeViewController.swift
+//  HubbyBuddy
+//
+//  Created by Donggeun Lee on 2022/01/19.
+//
+
+import Foundation
+import UIKit
+
+class SMSCodeViewController: UIViewController {
+    
+}
---
+++ HubbyBuddy/Views/SMSCodeView.swift
@@ -0,0 +1,8 @@
+//
+//  SMSCodeView.swift
+//  HubbyBuddy
+//
+//  Created by Donggeun Lee on 2022/01/19.
+//
+
+import Foundation
