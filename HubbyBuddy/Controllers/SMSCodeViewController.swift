9feae6ff75242dad19672afa3873de7c09058a8b
Donggeun Lee <patlee997@gmail.com>
2022-01-20 15:48:08 +0900

    SMSCode Verification process updated

--- HubbyBuddy/Controllers/SMSCodeViewController.swift
+++ HubbyBuddy/Controllers/SMSCodeViewController.swift
@@ -10,4 +10,35 @@ import UIKit
 
 class SMSCodeViewController: UIViewController {
     
+    let mainView = SMSCodeView()
+    var code: String = ""
+    
+    override func loadView() {
+        self.view = mainView
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mainView.codeTextField.addTarget(self, action: #selector(codeTextFieldDidChange(_:)), for: .editingChanged)
+    }
+    
+    @objc func codeTextFieldDidChange(_ textfield: UITextField) {
+        code = textfield.text ?? ""
+        print(code)
+    }
+    
+    @objc func nextBtnClicked() {
+        let number = code
+        print(number)
+        AuthManager.shared.verifyCode(smsCode: number) { [weak self] success in
+            guard success else { return }
+            DispatchQueue.main.async {
+                let vc = SignUpViewController()
+                vc.modalPresentationStyle = .fullScreen
+                self?.present(vc, animated: true)
+            }
+        }
+    }
 }
---
+++ HubbyBuddy/Controllers/SignUpViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SignUpViewController.swift
+//  HubbyBuddy
+//
+//  Created by Donggeun Lee on 2022/01/20.
+//
+
+import Foundation
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+}
