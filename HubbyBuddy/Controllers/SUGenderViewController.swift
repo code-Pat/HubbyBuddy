171cd919b4414dae8e408ed11782f538b0e104c8
Donggeun Lee <patlee997@gmail.com>
2022-01-25 00:10:26 +0900

    SUGenderViewController 뷰 탭 제스쳐 구현 & 버튼 색상 변경 구현

--- HubbyBuddy/Controllers/SUGenderViewController.swift
+++ HubbyBuddy/Controllers/SUGenderViewController.swift
@@ -20,6 +20,22 @@ class SUGenderViewController: UIViewController {
         super.viewDidLoad()
         
 //        mainView.nextButton.addTarget(self, action: #selector(nextBtnClicked), for: .touchUpInside)
+        
+        let tapGesture1: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tappedView1(_:)))
+        let tapGesture2: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tappedView2(_:)))
+        
+        mainView.maleView.addGestureRecognizer(tapGesture1)
+        mainView.femaleView.addGestureRecognizer(tapGesture2)
+    }
+    
+    @objc func tappedView1(_ gesture: UITapGestureRecognizer) {
+        mainView.maleView.backgroundColor = .green
+        mainView.nextButton.backgroundColor = .orange
+    }
+    
+    @objc func tappedView2(_ gesture: UITapGestureRecognizer) {
+        mainView.femaleView.backgroundColor = .green
+        mainView.nextButton.backgroundColor = .orange
     }
     
 //    @objc func nextBtnClicked() {
