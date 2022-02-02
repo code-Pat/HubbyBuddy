4a431376906f2ac29275067466f1934f93c25ae8
Donggeun Lee <patlee997@gmail.com>
2022-02-02 19:09:17 +0900

    Observable setup

---
+++ HubbyBuddy/Helpers/Observable.swift
@@ -0,0 +1,27 @@
+//
+//  Observable.swift
+//  HubbyBuddy
+//
+//  Created by Donggeun Lee on 2022/02/02.
+//
+
+import Foundation
+
+class Observable<T> {
+    private var listener: ((T) -> Void)?
+
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(_ closure: @escaping (T) -> Void) {
+        closure(value)
+        listener = closure
+    }
+}
