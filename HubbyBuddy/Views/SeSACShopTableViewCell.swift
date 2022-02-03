b63277cd89261df5635e6a5bd3c9880a791176ac
Donggeun Lee <patlee997@gmail.com>
2022-02-03 23:30:55 +0900

    SeSACShop - tableview 작업 셋업

---
+++ HubbyBuddy/Views/SeSACShopTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SeSACShopTableViewCell.swift
+//  HubbyBuddy
+//
+//  Created by Donggeun Lee on 2022/02/03.
+//
+
+import Foundation
+import UIKit
+
+class SeSACShopTableViewCell: UITableViewCell {
+    
+}
