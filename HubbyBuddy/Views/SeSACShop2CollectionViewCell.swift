fb56b5445e6328ba8d23977bb4f67dfb0243d1ca
Donggeun Lee <patlee997@gmail.com>
2022-02-07 23:41:19 +0900

    SeSACShop2View - ui setup

--- HubbyBuddy/Views/SeSACShop2CollectionViewCell.swift
+++ HubbyBuddy/Views/SeSACShop2CollectionViewCell.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+import SnapKit
 
 class SeSACShop2CollectionViewCell: UICollectionViewCell {
     
--- HubbyBuddy/Views/SeSACShop2View.swift
+++ HubbyBuddy/Views/SeSACShop2View.swift
@@ -7,7 +7,41 @@
 
 import Foundation
 import UIKit
+import SnapKit
 
 class SeSACShop2View: UIView {
+    var collectionView: UICollectionView = {
+        
+        var layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionInset = .zero
+        layout.itemSize = CGSize(width: UIScreen.main.bounds.width - 20, height: UIScreen.main.bounds.width / 2)
+        
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        cv.backgroundColor = .systemBackground
+        return cv
+    }()
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setupConstraints()
+       
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func setupConstraints() {
+        
+        addSubview(collectionView)
+        
+        collectionView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalTo(safeAreaLayoutGuide)
+        }
+    }
 }
