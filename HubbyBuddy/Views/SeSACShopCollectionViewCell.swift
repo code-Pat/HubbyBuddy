369a56dd78c9c07ea8fe19d324f5612fd3694000
Donggeun Lee <patlee997@gmail.com>
2022-02-07 16:41:19 +0900

    SeSACShopCollectionViewCell - collection view updated

--- HubbyBuddy/Views/SeSACShopCollectionViewCell.swift
+++ HubbyBuddy/Views/SeSACShopCollectionViewCell.swift
@@ -32,25 +32,26 @@ class SeSACShopCollectionViewCell: UICollectionViewCell {
     
     func setup() {
         
-        self.backgroundColor = .white
+        //self.backgroundColor = .white
         
-        imageView.image = UIImage(named: "sesac_face_01")
+        //imageView.image = UIImage(named: "sesac_face_01")
         imageView.contentMode = .scaleToFill
         
-        nameLabel.text = "기본 새싹"
+        //nameLabel.text = "기본 새싹"
         nameLabel.font = .systemFont(ofSize: 16)
         nameLabel.textColor = .black
         nameLabel.textAlignment = .left
         
-        pointLabel.text = "보유"
+        //pointLabel.text = "보유"
         pointLabel.font = .systemFont(ofSize: 14)
         pointLabel.textColor = .systemGray
         pointLabel.textAlignment = .center
         
-        descriptionLabel.text = "새싹을 대표하는 기본 식물입니다. 다른 새싹들과 함께 하는 것을 좋아합니다"
+        //descriptionLabel.text = "새싹을 대표하는 기본 식물입니다. 다른 새싹들과 함께 하는 것을 좋아합니다"
         descriptionLabel.font = .systemFont(ofSize: 14)
         descriptionLabel.textColor = .black
         descriptionLabel.textAlignment = .left
+        descriptionLabel.numberOfLines = 3
         
     }
     
@@ -64,7 +65,7 @@ class SeSACShopCollectionViewCell: UICollectionViewCell {
             make.top.equalToSuperview()
             make.leading.equalToSuperview()
             make.trailing.equalToSuperview()
-            make.height.equalTo(imageView.snp.width)
+            make.height.width.equalTo(160)
         }
         
         nameLabel.snp.makeConstraints { make in
@@ -75,10 +76,10 @@ class SeSACShopCollectionViewCell: UICollectionViewCell {
         }
         
         pointLabel.snp.makeConstraints { make in
-            make.top.equalTo(imageView.snp.bottom).offset(8)
+            make.centerY.equalTo(nameLabel)
             make.trailing.equalToSuperview()
             make.width.equalTo(40)
-            make.height.equalTo(20)
+            make.height.equalTo(30)
         }
         
         descriptionLabel.snp.makeConstraints { make in
